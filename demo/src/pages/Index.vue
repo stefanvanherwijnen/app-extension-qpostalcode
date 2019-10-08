d1413c1f5425002b99e88686c55c54d77c1d81eb
Stefan <stefan@devliert.info>
2019-10-08 18:46:13 +0200

    Demo

---
+++ demo/src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-form>
+    <q-postal-code
+      v-model="postalCode"
+      :rules="[val => !!val || 'Field is required']"
+      fill-mask
+      locale="nl"
+    ></q-postal-code>
+
+    <q-btn color="primary" type="submit" label="submit" />
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      postalCode: ''
+    }
+  }
+}
+</script>
